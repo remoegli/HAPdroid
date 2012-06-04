067949579661016a183800f0cd13905fd57139ae
Remo Egli <regli@hsr.ch>
2012-06-04 20:25:02 +0200

    added flow direction string representation

--- src/ch/hsr/hapdroid/graphlet/edge/DirectedEdge.java
+++ src/ch/hsr/hapdroid/graphlet/edge/DirectedEdge.java
@@ -1,16 +1,17 @@
 package ch.hsr.hapdroid.graphlet.edge;
 
+import org.anddev.andengine.entity.text.Text;
 import ch.hsr.hapdroid.graphlet.node.GraphletNode;
 import ch.hsr.hapdroid.transaction.Transaction;
 
 public class DirectedEdge extends LabeledEdge {
 
-	private DirectionType type;
-	
 	public DirectedEdge(GraphletNode left, GraphletNode right, Transaction trans) {
 		super(left, right, trans);
-		type = DirectionType.getType(trans);
-		this.setColor(type.getRed(), type.getGreen(), type.getBlue());
 	}
-
+	
+	protected Text getText(){
+		return new Text(30, 0, aFont, type.leftArrow() + transaction.getBytes() + "(" + transaction.getPackets() + ")" + type.rightArrow());
+	}
+		
 }
--- src/ch/hsr/hapdroid/graphlet/edge/LabeledEdge.java
+++ src/ch/hsr/hapdroid/graphlet/edge/LabeledEdge.java
@@ -8,17 +8,17 @@ import ch.hsr.hapdroid.transaction.Transaction;
 public class LabeledEdge extends BaseEdge {
 
 	protected Text edgeLabel;
+	protected Transaction transaction;
+	protected DirectionType type;
 	
 	public LabeledEdge(GraphletNode left, GraphletNode right, Transaction trans) {
 		super(left, right);
-		String packetsperflow;
-		if(trans.getFlows().size() > 0){
-			packetsperflow = Float.toString(trans.getPackets()/trans.getFlows().size());
-		} else {
-			packetsperflow = "-";
-		}
+		transaction = trans;
 		
-		edgeLabel = new Text(0, 0, aFont, trans.getFlows().size() + "(" + packetsperflow + ")");
+		type = DirectionType.getType(transaction);
+		this.setColor(type.getRed(), type.getGreen(), type.getBlue());
+		
+		edgeLabel = this.getText();
 		this.attachChild(edgeLabel);
 	}
 	
@@ -26,6 +26,16 @@ public class LabeledEdge extends BaseEdge {
 	public void update(){
 		super.update();
 		edgeLabel.setPosition((getX2()-getX1())/2 - (edgeLabel.getWidth()/2), ((getY2()-getY1())/2) - aFont.getLineHeight());
+	}
+	
+	protected Text getText(){
+		String packetsperflow;
+		if(transaction.getFlows().size() > 0){
+			packetsperflow = Float.toString(transaction.getPackets()/transaction.getFlows().size());
+		} else {
+			packetsperflow = "-";
+		}
+		return new Text(0, 0, aFont, transaction.getFlows().size() + "(" + packetsperflow + ")");
 	}
 
 }
